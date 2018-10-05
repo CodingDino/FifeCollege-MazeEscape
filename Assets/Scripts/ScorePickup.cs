28277f6bf4aabf17a525b847812278152054a7c9
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-10-05 14:56:24 +0100

    Added tile map, score items, camera follow

---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+    public Score scoreTracker;
+    public int scoreValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Only do stuff if it's the player we've hit
+        if (collision.GetComponent<Player>())
+        {
+            // Updating the score based on how much this item is worth
+            scoreTracker.ChangeValue(scoreValue);
+
+            // get rid of the item (it's been used up)
+            Destroy(gameObject);
+        }
+    }
+
+}
